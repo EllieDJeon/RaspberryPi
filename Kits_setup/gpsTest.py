b2608efe4286173bd97ea65b9054424546b01d2d
Ellie <elliejeon92@gmail.com>
2018-06-22 14:29:53 -0500

    Update gpsTest.py

--- Kits_setup/gpsTest.py
+++ Kits_setup/gpsTest.py
@@ -7,10 +7,22 @@ while True:
 	data = line.split(",")
 	
 	if data[0] == '$GPRMC' and data[2] == "A":
-		gpsdata = {"TimeStamp":data[1], "Latitude":data[3], "Longitude":data[5], "NS":data[4], "EW":data[6]}
-                gpsList.append(gpsdata)
-		print(len(gpsList))
-		if len(gpsList) == 10:
+		gpsdata = {"TimeStamp":data[1], "Latitude":data[3],"NS":data[4], "Longitude":data[5],"EW":data[6]}
+	if data[0] == '$GPGGA' : 
+		gpsdata["Altitude"] = data[7]
+		gpsdata["Satellites"] = data[5] # 5 satellites are in view
+	if data[0] == '$GPHDT': gpsdata["Heading"] = data[1]
+	if data[0] == '$GPVTG' : gpsdata["Speed_km"] = data[7] # speed over ground in kilometer/hour
+		# data[5] = Speed over ground in knots
+        gpsList.append(gpsdata)
+	print(len(gpsList))
+	
+	if len(gpsList) == 10:
+		with open("GPS_DATA.json", mode='a') as f:
+		json.dump(gpsList, f)
+		f.write("\n")
+		gpsList = []
+		print("File Successed")
 
 #			with open("GPS_DATA.json", mode='a') as f:
 #				for g in gpsList:
@@ -20,8 +32,4 @@ while True:
 #                               print("File Successed")
 
 
-			with open("GPS_DATA.json", mode='a') as f:
-				json.dump(gpsList, f)
-				f.write("\n")
-				gpsList = []
-				print("File Successed")
+
