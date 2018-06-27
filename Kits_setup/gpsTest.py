92d1e97fb7045b86493dd0c63234844bfe82dfdd
Ellie <33610035+EllieDJeon@users.noreply.github.com>
2018-06-26 21:05:06 -0500

    Update gpsTest.py

--- Kits_setup/gpsTest.py
+++ Kits_setup/gpsTest.py
@@ -41,13 +41,19 @@ while True:
         line = gps.readline()
         data = line.split(",")
 
-        if data[0] == '$GPRMC' and data[2] == "A":
-                gpsdata = {"TimeStamp":data[1], "Latitude":data[3],"NS":data[4], "Longitude":data[5],"EW":data[6]}
-	if data[0] == '$GPGGA' : 
-		gpsdata["Altitude"] = data[7]
-		gpsdata["Satellites"] = data[5] # 5 satellites are in view
-	if data[0] == '$GPHDT': gpsdata["Heading"] = data[1]
-	if data[0] == '$GPVTG' : gpsdata["Speed_km"] = data[7] # speed over ground in kilometer/hour
+        if data[0] == '$GPRMC' and data[2] == "A": 
+            gpsdata['TimeStamp'] = data[1]
+            gpsdata['Latitude'] = data[3]
+            gpsdata['NS'] = data[4]
+            gpsdata['Longitude'] = data[5]
+            gpsdata['EW'] = data[6]
+            gpsdata['Speed_hp'] = data[7]
+            
+        if data[0] == '$GPGGA':
+            gpsdata['TimeStamp'] = data[1]
+            gpsdata['Altitude'] = data[7]
+            gpsdata['Satellites'] = data[5] # 5 satellites are in view
+        if data[0] == '$GPHDT': gpsdata['Heading'] = data[1]
 		# data[5] = Speed over ground in knots
 	print(gpsdata)
 #               gpsList.append(gpsdata)
