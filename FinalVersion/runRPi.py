d32d6bee72a1e20f76d7a6eb38941d7e167b1fed
Ellie <33610035+EllieDJeon@users.noreply.github.com>
2018-07-24 19:38:44 +0000

    Add files via upload

---
+++ FinalVersion/runRPi.py
@@ -0,0 +1,5 @@
+import os
+#os.system("python basicPubSub_topic.py -e azozmhv0nd77.iot.us-west-2.amazonaws.com -r root_CA.pem -c 3ddd7abd2c-certificate.pem.crt -k 3ddd7abd2c-private.pem.key")
+#os.system("python basicPubSub_Camera.py -e azozmhv0nd77.iot.us-west-2.amazonaws.com -r root_CA.pem -c 3ddd7abd2c-certificate.pem.crt -k 3ddd7abd2c-private.pem.key")
+os.system("python basicPubSub_GPS_v8.py -e azozmhv0nd77.iot.us-west-2.amazonaws.com -r root_CA.pem -c 3ddd7abd2c-certificate.pem.crt -k 3ddd7abd2c-private.pem.key")
+
